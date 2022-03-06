23c0f964f1985a1a0148e8683ac585e85c248f0c
mohamedmohesn <82167985+mohamedmohesn@users.noreply.github.com>
2022-03-06 20:41:28 +0200

    Delete chat-kit--app directory

--- chat-kit--app/routes/pharmcyRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const authController = require('../controllers/pharmcyController');
-
-const router = Router();
-
-router.post('/addpharmcy',authController.Add_pharmcy);
-router.get('/findall',authController.findALL);
-
-module.exports = router;
